4d7dc94e712b2e958421dd0b99ee7de2399f88cb
Veronika Shirochenkova <86922246+VeronikaShirochenkova@users.noreply.github.com>
2023-03-11 17:46:15 +0100

    #2 Device-based to action-based

---
+++ libraryVR/Assets/Scripts/HandController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField] private XRRayInteractor _xrRayInteractor;
+    [SerializeField] private ActionBasedController _actionBasedController;
+    [SerializeField] private XRDirectInteractor _xrDirectInteractor;
+    [SerializeField] private InputActionReference _teleportActionRef;
+    
+    private void OnEnable()
+    {
+        _teleportActionRef.action.performed += teleportModeActivate;
+        _teleportActionRef.action.canceled += teleportModeCancel;
+
+    }
+    
+    private void OnDisable()
+    {
+        _teleportActionRef.action.performed -= teleportModeActivate;
+        _teleportActionRef.action.canceled -= teleportModeCancel;
+    }
+
+    private void teleportModeActivate(InputAction.CallbackContext obj)
+    {
+        _xrDirectInteractor.enabled = false;
+
+        _xrRayInteractor.enabled = true;
+        _actionBasedController.enableInputActions = true;
+    }
+
+    private void teleportModeCancel(InputAction.CallbackContext obj) => Invoke("DisableTeleport", 0.05f);
+
+    private void DisableTeleport()
+    {
+        _xrDirectInteractor.enabled = true;
+
+        _xrRayInteractor.enabled = false;
+        _actionBasedController.enableInputActions = false;
+    }
+
+
+}
---
+++ libraryVR/Assets/Scripts/UIController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private XRRayInteractor _xrRayInteractor;
+    [SerializeField] private ActionBasedController _actionBasedController;
+    [SerializeField] private XRDirectInteractor _xrDirectInteractor;
+    [SerializeField] private InputActionReference _UIActionRef;
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        _UIActionRef.action.performed += _UIModeActivate;
+        _UIActionRef.action.canceled += _UIModeCancel;
+
+    }
+
+    private void _UIModeActivate(InputAction.CallbackContext obj)
+    {
+        _xrDirectInteractor.enabled = false;
+
+        _xrRayInteractor.enabled = true;
+        _actionBasedController.enableInputActions = true;
+    }
+
+    private void _UIModeCancel(InputAction.CallbackContext obj) => Invoke("DisableUI", 0.05f);
+
+    private void DisableUI()
+    {
+        _xrDirectInteractor.enabled = true;
+
+        _xrRayInteractor.enabled = false;
+        _actionBasedController.enableInputActions = false;
+    }
+
+    private void OnDisable()
+    {
+        _UIActionRef.action.performed -= _UIModeActivate;
+        _UIActionRef.action.canceled -= _UIModeCancel;
+    }
+}
