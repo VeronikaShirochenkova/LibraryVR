41261d7c447ddc79cf1e4c2f9d3d5ea823dbc970
Veronika Shirochenkova <86922246+VeronikaShirochenkova@users.noreply.github.com>
2023-03-11 15:43:36 +0100

    #1 Basic setup + teleportation

---
+++ libraryVR/Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController teleportRay;
+    public InputHelpers.Button teleportBtn;
+    
+    public XRController uiRay;
+    public InputHelpers.Button uiBtn;
+    
+    public float threshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool btnIsPressed = TeleportBtnIsPressed(teleportRay);
+        teleportRay.gameObject.SetActive(btnIsPressed);
+        
+        btnIsPressed = UIbtnIsPressed(teleportRay);
+        uiRay.gameObject.SetActive(btnIsPressed);
+    }
+
+    public bool TeleportBtnIsPressed(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportBtn, out bool isPressed, threshold);
+        return isPressed;
+    }
+    
+    public bool UIbtnIsPressed(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, uiBtn, out bool isPressed, threshold);
+        return isPressed;
+    }
+}
